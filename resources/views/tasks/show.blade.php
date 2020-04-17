ca441b5e9fc66c5855cff3d0e1cd15bf5d8ca0b5
takeshi.ishimaru <tkcglutamine38@gmail.com>
2020-04-17 10:03:21 +0000

    add column validation

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,6 +9,8 @@
             <th>id</th>
             <td>{{ $task->id }}</td>
         </tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
         <tr>
             <th>メッセージ</th>
             <td>{{ $task->content }}</td>
